8fbec1c120a0e4ec09f518eb717887c838c5d94f
Dan <dansonlinepresence@gmail.com>
2019-03-26 22:58:49 -0400

    models for characters, spells, and offers

--- dnd_weave_app/models.py
+++ dnd_weave_app/models.py
@@ -14,3 +14,18 @@ class Secret(models.Model):
     serialized = models.TextField()
 
     objects = SecretManager()
+
+class Character(models.Model):
+    name = models.TextField()
+    player = models.ForeignKey(User, models.CASCADE)
+    secret = models.ForeignKey(Secret, models.PROTECT, null=True)
+
+class Spell(models.Model):
+    character = models.ForeignKey(Character, models.CASCADE)
+    ciphertext = models.BinaryField()
+    learned = models.BooleanField()
+    level = models.IntegerField()
+
+class Offer(models.Model):
+    secret = models.ForeignKey(Secret, models.CASCADE)
+    character = models.ForeignKey(Character, models.CASCADE)
